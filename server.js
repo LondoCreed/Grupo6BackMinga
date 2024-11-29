bcb3a3abc65b5b7dc8598a1f5f558db41b8db4b7
jhoselerodriguez <jhoselerodriguez@gmail.com>
2024-11-29 17:52:25 -0500

    guardado2

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ import "./config/database.js"
 import cors from "cors"
 import morgan from "morgan"
 import indexRouter from "./router/index.js"
-
-/* 
 import not_found_handler from "./middlewares/not_found_handler.js"
 import error_handler from "./middlewares/error_handler.js"
-import error_400 from "./middlewares/error_400.js"
- */
+import bad_request_handler from "./middlewares/bad_request_handler.js"
+
 const server = express()
 const PORT = process.env.PORT || 8080
 const ready = () => console.log("server ready in port:"+PORT)
@@ -17,11 +15,11 @@ const ready = () => console.log("server ready in port:"+PORT)
 server.use(express.json()) //permite trabajar con formato json en entrada y salida
 server.use(express.urlencoded({ extended: true })) //permite por medio de la url recibir peticiones complejas o sea recibir queries o parametros
 server.use(cors()) //no deja pasar a cualquiera
-server.use(morgan('dev')) 
+server.use(morgan('dev'))
 server.use('/api', indexRouter)
-/* 
+server.use(bad_request_handler)
 server.use(not_found_handler)
 server.use(error_handler)
-server.use(error_400)
- */
+
+
 server.listen(PORT, ready)
