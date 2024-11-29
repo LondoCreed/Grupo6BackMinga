71c041b99d23eda45e417da0a782f748beb10726
Alejandro Vargas <alejovastillo@gmail.com>
2024-11-29 09:08:56 -0500

    Joi for register and middleware validator

--- router/user.js
+++ router/user.js
@@ -6,10 +6,16 @@ import registerUser from "../controllers/user/register.js";
 import updateUser from "../controllers/user/update.js";
 import deleteUser from "../controllers/user/delete.js";
 
+//middlewares
+import validator from "../middlewares/validator.js";
+
+//schemas
+import registerSchema from "../schemas/users/register.js";
+
 const router = Router()
 
 router.get('/all', allUsers)
-router.post('/register', registerUser)
+router.post('/register', validator(registerSchema), registerUser)
 router.put('/update', updateUser)
 router.delete('/delete', deleteUser)
 
