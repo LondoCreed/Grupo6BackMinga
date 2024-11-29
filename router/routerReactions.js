ae0aca187b2eecdd1889ad1e4462b3df00b4f207
Carlos Rene Perez G <d.londocreed@gmail.com>
2024-11-29 09:19:55 -0600

    CRUD Reactions y Middle Validator

---
+++ router/routerReactions.js
@@ -0,0 +1,18 @@
+import { Router } from "express"
+import createReaction from '../controllers/reactions/create.js';
+import deleteReaction from '../controllers/reactions/delete.js';
+import { allReactions, reactionById } from '../controllers/reactions/read.js';
+import updateReaction from '../controllers/reactions/update.js';
+import validator from "../middlewares/validator.js";
+import reactionSchema from "../schemas/reactionSchema.js";
+
+
+const routerReactions = Router()
+
+routerReactions.post('/create', validator(reactionSchema), createReaction);
+routerReactions.get('/', allReactions);
+routerReactions.get('/id/:id', reactionById);
+routerReactions.put('/update/:id', updateReaction);
+routerReactions.delete('/delete/:id', deleteReaction);
+
+export default routerReactions 
