2be061ef8b916a8692dc4998dd8e3bcddae9c42d
Alejandro Vargas <alejovastillo@gmail.com>
2024-11-30 11:19:04 -0500

    comments to produc

--- router/comments.js
+++ router/comments.js
@@ -1,16 +1,16 @@
 import { Router } from "express"
-// import { allComments } from "../controllers/comments/read.js"
-// import { commentCreated } from "../controllers/comments/create.js"
-// import { updateComment } from "../controllers/comments/update.js"
-// import { deletedComment } from "../controllers/comments/deleted.js"
-// import validator from "../middlewares/validator.js"
-// import schemaComment from "../schemas/comment/create.js"
+import { allComments } from "../controllers/comments/read.js"
+import { commentCreated } from "../controllers/comments/create.js"
+import { updateComment } from "../controllers/comments/update.js"
+import { deletedComment } from "../controllers/comments/deleted.js"
+import validator from "../middlewares/validator.js"
+import schemaComment from "../schemas/comment/create.js"
 
 const routerComment = Router()
 
-// routerComment.get("/all", allComments)
-// routerComment.post("/create",validator(schemaComment), commentCreated)
-// routerComment.put("/update", updateComment)
-// routerComment.delete("/delete", deletedComment)
+routerComment.get("/all", allComments)
+routerComment.post("/create",validator(schemaComment), commentCreated)
+routerComment.put("/update", updateComment)
+routerComment.delete("/delete", deletedComment)
 
 export default routerComment
