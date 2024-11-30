2a7b9831b3746ca468135f9f985c4929d8d4d3fd
Alejandro Vargas <alejovastillo@gmail.com>
2024-11-30 10:43:01 -0500

    Try fix 1

--- router/categories.js
+++ router/categories.js
@@ -1,16 +1,16 @@
 import { Router } from "express"
-import { categoryCreated } from "../controllers/categories/create.js"
-import { allCategories } from "../controllers/categories/read.js"
-import {updateCategory} from "../controllers/categories/update.js"
-import {deletedCategory} from "../controllers/categories/deleted.js"
+// import { categoryCreated } from "../controllers/categories/create.js"
+// import { allCategories } from "../controllers/categories/read.js"
+// import {updateCategory} from "../controllers/categories/update.js"
+// import {deletedCategory} from "../controllers/categories/deleted.js"
 import validator from "../middlewares/validator.js"
 import schemaCategory from "../schemas/category/create.js"
 
 const routerCategory = Router()
 
-routerCategory.get("/all", allCategories)
-routerCategory.post("/create", validator(schemaCategory), categoryCreated)
-routerCategory.put("/update", updateCategory)
-routerCategory.delete("/delete", deletedCategory)
+// routerCategory.get("/all", allCategories)
+// routerCategory.post("/create", validator(schemaCategory), categoryCreated)
+// routerCategory.put("/update", updateCategory)
+// routerCategory.delete("/delete", deletedCategory)
 
 export default routerCategory
