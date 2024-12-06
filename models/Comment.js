a67e92a57bcca3a10fb06351185e23a26b570e0b
Alejandro Vargas <alejovastillo@gmail.com>
2024-12-05 20:36:58 -0500

    add coments by  chapter

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,7 @@ import { Schema,model } from "mongoose";
 let collection = "comments"
 
 let schema = new Schema({
-    charperId:{type:Schema.Types.ObjectId, ref:"charpters", required:true},
+    chapterId:{type:Schema.Types.ObjectId, ref:"chapters", required:true},
     authorId:{type:Schema.Types.ObjectId, ref:"authors", required:false},
     companyId:{type:Schema.Types.ObjectId, ref:"companies", required:false},
     message:{type:String,required:true}
